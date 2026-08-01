dbad851cd0f9359c5d56e4bb5472464702178bcb
saxenaaradhy <saxenaaradhya2007@gmail.com>
2026-08-01 19:20:04 +0530

    jlj

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -3,10 +3,9 @@ import { doc, setDoc, increment } from 'firebase/firestore'
 
 export async function trackEvent(type, data, userId = null) {
   try {
-    const ref = doc(db, 'analytics', data.sessionId)
+    const ref = doc(db, 'analytics', data.categoryId, 'sessions', data.sessionId)
     await setDoc(ref, {
       sessionTitle: data.sessionTitle,
-      categoryId: data.categoryId,
       [`${type}Count`]: increment(1),
       lastUpdated: new Date().toISOString(),
     }, { merge: true })
