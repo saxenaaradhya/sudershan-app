18df38d5d7e8e925faecacece6b9eb99dd7cf168
saxenaaradhy <saxenaaradhya2007@gmail.com>
2026-08-09 21:44:20 +0530

    sdf

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -91,6 +91,10 @@ export default function HomePage() {
                 <span style={{ color: '#FF8C42' }}>Mr. Sandeep</span>
               </h2>
 
+              <h1 className="text-lg sm:text-3xl font-bold mb-2 leading-snug" style={{ color: '#FF8C42' }}>
+                SANDEEP
+              </h1>
+
               <p className="text-xs sm:text-sm mb-4 leading-relaxed" style={{ color: '#fafa07' }}>
                 Hypnothrapist and reiki Grandmaster
               </p>
